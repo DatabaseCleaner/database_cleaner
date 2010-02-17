43c5a2dd88d2f124191801e6d385cc2014267d9d
Darrin Holst <darrinholst@gmail.com>
2010-02-17 13:49:40 -0600

    fall back to delete if truncate doesn't work in the jdbc adapter (when using sqlite3 via jdbc)

--- lib/database_cleaner/active_record/truncation.rb
+++ lib/database_cleaner/active_record/truncation.rb
@@ -17,7 +17,11 @@ module ActiveRecord
 
     class JdbcAdapter
       def truncate_table(table_name)
-        execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
+        begin
+          execute("TRUNCATE TABLE #{quote_table_name(table_name)};")
+        rescue ActiveRecord::StatementInvalid
+          execute("DELETE FROM #{quote_table_name(table_name)};")
+        end
       end
     end
 
