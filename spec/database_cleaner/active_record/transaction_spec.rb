3737e8f146b4431ecf6f9a6f8cf440eac671069b
Ben Mabey <ben@benmabey.com>
2010-03-29 09:42:29 -0600

    removes whitespace and dead comment

--- spec/database_cleaner/active_record/transaction_spec.rb
+++ spec/database_cleaner/active_record/transaction_spec.rb
@@ -8,7 +8,6 @@ module DatabaseCleaner
     describe Transaction do
       let (:connection) { mock("connection") }
       before(:each) do
-        #connection.stub!(:disable_referential_integrity).and_yield
         ::ActiveRecord::Base.stub!(:connection).and_return(connection)
       end
 
@@ -16,27 +15,27 @@ module DatabaseCleaner
         it "should increment open transactions if possible" do
           connection.stub!(:respond_to?).with(:increment_open_transactions).and_return(true)
           connection.stub!(:begin_db_transaction)
-          
+
           connection.should_receive(:increment_open_transactions)
           Transaction.new.start
         end
-        
+
         it "should tell ActiveRecord to increment connection if its not possible to increment current connection" do
           connection.stub!(:respond_to?).with(:increment_open_transactions).and_return(false)
           connection.stub!(:begin_db_transaction)
-          
+
           ::ActiveRecord::Base.should_receive(:increment_open_transactions)
           Transaction.new.start
         end
-        
+
         it "should start a transaction" do
             connection.stub!(:increment_open_transactions)
-            
+
             connection.should_receive(:begin_db_transaction)
             Transaction.new.start
         end
       end
-      
+
       describe "clean" do
         it "should start a transaction" do
             connection.stub!(:decrement_open_transactions)
