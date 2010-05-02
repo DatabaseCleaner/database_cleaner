253247bd0813dc39c9c493a80119a6403f13ee2d
Ben Mabey <ben@benmabey.com>
2010-05-02 12:40:30 -0600

    Updates the DataMapper truncation strategy to version 0.10.3

--- lib/database_cleaner/data_mapper/truncation.rb
+++ lib/database_cleaner/data_mapper/truncation.rb
@@ -29,7 +29,7 @@ module DataMapper
           execute("SET FOREIGN_KEY_CHECKS = 0;")
           yield
         ensure
-          execute("SET FOREIGN_KEY_CHECKS = #{old};")
+          execute("SET FOREIGN_KEY_CHECKS = #{old.first};")
         end
       end
 
