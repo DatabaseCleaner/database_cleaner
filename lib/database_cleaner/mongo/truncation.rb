9323d6c3a06b09a711e32f9949fec709602f17d8
Ben Mabey <ben@benmabey.com>
2011-01-26 09:38:19 -0700

    drop indexes for mongo collections before removing them

--- lib/database_cleaner/mongo/truncation.rb
+++ lib/database_cleaner/mongo/truncation.rb
@@ -4,15 +4,20 @@ module DatabaseCleaner
       
       def clean
         if @only
-          collections.each { |c| c.remove if @only.include?(c.name) }
+          collections.each { |c| remove(c) if @only.include?(c.name) }
         else
-          collections.each { |c| c.remove unless @tables_to_exclude.include?(c.name) }
+          collections.each { |c| remove(c) unless @tables_to_exclude.include?(c.name) }
         end
         true
       end
 
       private
       
+      def remove(collection)
+        collection.drop_indexes
+        collection.remove
+      end
+
       def collections
         database.collections.select { |c| c.name !~ /^system/ }
       end
