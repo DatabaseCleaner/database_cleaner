8c13f57a7a08029a00605e152f3a3f212d4dbf89
Ben Mabey <ben@benmabey.com>
2011-02-04 08:51:39 -0700

    removes dropping of mongo indexes, see url below for more info
    https://github.com/bmabey/database_cleaner/commit/9323d6c3a06b09a711e32f9949fec709602f17d8

--- lib/database_cleaner/mongo/truncation.rb
+++ lib/database_cleaner/mongo/truncation.rb
@@ -1,22 +1,17 @@
 module DatabaseCleaner
   module Mongo
     module Truncation
-      
+
       def clean
         if @only
-          collections.each { |c| remove(c) if @only.include?(c.name) }
+          collections.each { |c| c.remove if @only.include?(c.name) }
         else
-          collections.each { |c| remove(c) unless @tables_to_exclude.include?(c.name) }
+          collections.each { |c| c.remove unless @tables_to_exclude.include?(c.name) }
         end
         true
       end
 
       private
-      
-      def remove(collection)
-        collection.drop_indexes
-        collection.remove
-      end
 
       def collections
         database.collections.select { |c| c.name !~ /^system/ }
