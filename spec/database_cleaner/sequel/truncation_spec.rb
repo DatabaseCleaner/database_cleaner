29d70b79ebe58066c5b55c3510b5d6ac4a41d4e0
Corin Langosch <info@netskin.com>
2011-03-30 10:27:03 +0200

    added basic specs

---
+++ spec/database_cleaner/sequel/truncation_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'database_cleaner/sequel/truncation'
+require 'database_cleaner/shared_strategy_spec'
+require 'sequel'
+
+module DatabaseCleaner
+  module Sequel
+    describe Truncation do
+      it_should_behave_like "a generic strategy"
+      it_should_behave_like "a generic truncation strategy"
+    end
+  end
+end
