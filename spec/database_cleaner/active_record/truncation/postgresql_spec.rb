347f376c2de944be0f979ce1835c50b03708a016
Ben Mabey <ben@benmabey.com>
2012-08-05 15:44:31 -0600

    refactors PG tests not to mock itself, use main API method instead

--- spec/database_cleaner/active_record/truncation/postgresql_spec.rb
+++ spec/database_cleaner/active_record/truncation/postgresql_spec.rb
@@ -19,83 +19,61 @@ module ActiveRecord
       end
 
       describe "#truncate_table" do
-        it "should truncate the table" do
+        it "truncates the table" do
           2.times { User.create }
 
           connection.truncate_table('users')
           User.count.should == 0
         end
 
-        it "should reset AUTO_INCREMENT index of table" do
+        it "resets AUTO_INCREMENT index of table" do
           2.times { User.create }
           User.delete_all
 
           connection.truncate_table('users')
 
           User.create.id.should == 1
-        end
-      end
-
-      describe "#truncate_tables_with_id_reset" do
-        it "responds" do
-          adapter.instance_methods.should include('truncate_tables_with_id_reset')
-        end
-
-        it "should truncate the table" do
-          2.times { User.create }
-
-          connection.truncate_tables_with_id_reset(['users'])
-          User.count.should be_zero
-        end
-
-        it "should reset AUTO_INCREMENT index of table" do
-          2.times { User.create }
-          User.delete_all
-
-          connection.truncate_tables_with_id_reset(['users'])
-
-          User.create.id.should == 1
-        end
-      end
-
-      describe "#truncate_tables_no_id_reset" do
-        it "responds" do
-          adapter.instance_methods.map(&:to_s).should include('truncate_tables_no_id_reset')
-        end
-
-        it "should truncate the table" do
-          2.times { User.create }
-
-          connection.truncate_tables_no_id_reset(['users'])
-          User.count.should == 0
-        end
-
-        it "should not reset AUTO_INCREMENT index of table" do
-          2.times { User.create }
-          User.delete_all
-
-          connection.truncate_tables_no_id_reset(['users'])
-
-          User.create.id.should == 3
         end
       end
 
       describe "#fast_truncate_tables" do
-        it "responds" do
-          adapter.instance_methods.should include('fast_truncate_tables')
+        
+        context "with :reset_ids set true" do
+          it "truncates the table" do
+            2.times { User.create }
+
+            connection.fast_truncate_tables(%w[users], :reset_ids => true)
+            User.count.should be_zero
+          end
+
+          it "resets AUTO_INCREMENT index of table" do
+            2.times { User.create }
+            User.delete_all
+
+            connection.fast_truncate_tables(%w[users]) # true is also the default
+            User.create.id.should == 1
+          end
         end
 
-        it 'should call #truncate_tables_with_id_reset on each table if :reset_ids option true was given' do
-          connection.should_receive(:truncate_tables_with_id_reset).exactly(connection.tables.size).times
+        
+        context "with :reset_ids set false" do
+          it "truncates the table" do
+            2.times { User.create }
 
-          connection.fast_truncate_tables(connection.tables)
+            connection.fast_truncate_tables(%w[users], :reset_ids => false)
+            User.count.should == 0
+          end
+
+          it "does not reset AUTO_INCREMENT index of table" do
+            2.times { User.create }
+            User.delete_all
+
+            connection.fast_truncate_tables(%w[users], :reset_ids => false)
+
+            User.create.id.should == 3
+          end
         end
-
-        it 'should call #truncate_tables_with_id_reset on each table if :reset_ids option false was given' do
-          connection.should_receive(:truncate_tables_no_id_reset).exactly(connection.tables.size).times
-
-          connection.fast_truncate_tables(connection.tables, :reset_ids => false)
-        end
+        
       end
     end
   end
