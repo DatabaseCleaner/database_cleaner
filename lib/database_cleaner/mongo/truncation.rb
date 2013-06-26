d475f01c29331af6fa46a3c416e69b5f874ec742
Judson <nyarly@gmail.com>
2013-06-26 14:59:58 -0700

    Prevent cacheing of new databases

--- lib/database_cleaner/mongo/truncation.rb
+++ lib/database_cleaner/mongo/truncation.rb
@@ -17,8 +17,30 @@ module DatabaseCleaner
         @@collections_cache ||= {}
       end
 
+      def mongoid_collection_names
+        @@mongoid_collection_names ||= Hash.new{|h,k| h[k]=[]}.tap do |names|
+          ObjectSpace.each_object(Class) do |klass|
+            next unless klass.ancestors.include?(::Mongoid::Document)
+            next if klass.embedded
+            next if klass.collection_name.empty?
+            names[klass.db.name] << klass.collection_name
+          end
+        end
+      end
+
       def collections
-        collections_cache[database.name] ||= database.collections.select { |c| c.name !~ /^system\./ }
+        return collections_cache[database.name] if collections_cache.has_key?(database.name)
+        db_collections = database.collections.select { |c| c.name !~ /^system\./ }
+
+        missing_collections = db_collections.map(&:name) - mongoid_collection_names[database.name]
+
+        if missing_collections.empty?
+          collections_cache[database.name] = db_collections
+        else
+          puts "Not cacheing collection names. Missing these from models: #{missing_collections.inspect}"
+        end
+
+        return db_collections
       end
     end
   end
