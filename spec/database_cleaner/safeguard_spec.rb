bec3f64b78ce919b9cd9ddfadefb17c2aae2454b
Sven Fuchs <me@svenfuchs.com>
2018-03-24 16:14:10 +0100

    add safeguard for running in production, namespace errors, add error messages

--- spec/database_cleaner/safeguard_spec.rb
+++ spec/database_cleaner/safeguard_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
+require 'support/env'
 require 'active_record'
 require 'database_cleaner/active_record/transaction'
 
 module DatabaseCleaner
   describe Safeguard do
+    include Support::Env
+
     let(:strategy) { DatabaseCleaner::ActiveRecord::Transaction }
     let(:cleaner)  { Base.new(:autodetect) }
 
@@ -11,29 +14,64 @@ module DatabaseCleaner
 
     describe 'DATABASE_URL is set' do
       describe 'to any value' do
-        before { ENV['DATABASE_URL'] = 'postgres://remote.host' }
-        after  { ENV.delete('DATABASE_URL') }
+        env DATABASE_URL: 'postgres://remote.host'
 
-        it 'raises DatabaseUrlSpecified' do
-          expect { cleaner.start }.to raise_error(DatabaseUrlSpecified)
+        it 'raises' do
+          expect { cleaner.start }.to raise_error(Safeguard::Error::RemoteDatabaseUrl)
         end
       end
 
       describe 'to a local url' do
-        before { ENV['DATABASE_URL'] = 'postgres://localhost' }
-        after  { ENV.delete('DATABASE_URL') }
+        env DATABASE_URL: 'postgres://localhost'
 
         it 'does not raise' do
           expect { cleaner.start }.to_not raise_error
         end
       end
 
-      describe 'DATABASE_CLEANER_SKIP_SAFEGUARD is set' do
-        before { ENV['DATABASE_CLEANER_SKIP_SAFEGUARD'] = 'true' }
-        after  { ENV.delete('DATABASE_CLEANER_SKIP_SAFEGUARD') }
+      describe 'DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL is set' do
+        env DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL: true
 
         it 'does not raise' do
           expect { cleaner.start }.to_not raise_error
+        end
+      end
+
+      describe 'DatabaseCleaner.allow_remote_database_url is true' do
+        before { DatabaseCleaner.allow_remote_database_url = true }
+        after  { DatabaseCleaner.allow_remote_database_url = nil }
+
+        it 'does not raise' do
+          expect { cleaner.start }.to_not raise_error
+        end
+      end
+    end
+
+    describe 'ENV is set to production' do
+      %w(ENV RACK_ENV RAILS_ENV).each do |key|
+        describe "on #{key}" do
+          env key => 'production'
+
+          it 'raises' do
+            expect { cleaner.start }.to raise_error(Safeguard::Error::ProductionEnv)
+          end
+        end
+
+        describe 'DATABASE_CLEANER_ALLOW_PRODUCTION is set' do
+          env DATABASE_CLEANER_ALLOW_PRODUCTION: true
+
+          it 'does not raise' do
+            expect { cleaner.start }.to_not raise_error
+          end
+        end
+
+        describe 'DatabaseCleaner.allow_production is true' do
+          before { DatabaseCleaner.allow_production = true }
+          after  { DatabaseCleaner.allow_production = nil }
+
+          it 'does not raise' do
+            expect { cleaner.start }.to_not raise_error
+          end
         end
       end
     end
