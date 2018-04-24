ac3db9b7728e15c0ed9e152cca74d0df284ec4dd
Micah Geisel <micah@botandrose.com>
2018-04-23 18:19:21 -0700

    is this what's going on? wish i could reproduce locally...

--- lib/database_cleaner/mongo/truncation_mixin.rb
+++ lib/database_cleaner/mongo/truncation_mixin.rb
@@ -8,6 +8,7 @@ module DatabaseCleaner
         else
           collections.each { |c| c.send(truncate_method_name) unless @tables_to_exclude.include?(c.name) }
         end
+        wait_for_truncations_to_finish
         true
       end
 
@@ -20,6 +21,10 @@ module DatabaseCleaner
       def truncate_method_name
         # This constant only exists in the 2.x series.
         defined?(::Mongo::VERSION) ? :delete_many : :remove
+      end
+
+      def wait_for_truncations_to_finish
+        database.get_last_error
       end
     end
   end
