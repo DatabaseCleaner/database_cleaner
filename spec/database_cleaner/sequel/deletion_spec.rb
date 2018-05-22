1569b4681e36ad98d77e931585e4614a68d06bfc
Micah Geisel <micah@botandrose.com>
2018-05-22 06:14:31 -0700

    refactor sequel deletion spec to use sequel helper and test all four database connections.

--- spec/database_cleaner/sequel/deletion_spec.rb
+++ spec/database_cleaner/sequel/deletion_spec.rb
@@ -6,59 +6,42 @@ module DatabaseCleaner
   module Sequel
     RSpec.describe Deletion do
       it_should_behave_like "a generic strategy"
-    end
 
-    RSpec.shared_examples 'a Sequel deletion strategy' do
-      let(:deletion) do
-        d = Deletion.new
-        d.db = db
-        d
-      end
+      [
+        { url: 'mysql:///',    connection_options: db_config['mysql'] },
+        { url: 'mysql2:///',   connection_options: db_config['mysql2'] },
+        { url: 'sqlite:///',   connection_options: db_config['sqlite3'] },
+        { url: 'postgres:///', connection_options: db_config['postgres'] },
+      ].each do |config|
+        context "using a #{config[:url]} connection" do
+          let(:helper) { SequelHelper.new(config) }
 
-      context 'when several tables have data' do
-        before(:each) do
-          db.create_table!(:precious_stones) { primary_key :id }
-          db.create_table!(:replaceable_trifles)  { primary_key :id }
-          db.create_table!(:worthless_junk)  { primary_key :id }
+          around do |example|
+            helper.setup
+            example.run
+            helper.teardown
+          end
 
-          db[:precious_stones].insert
-          db[:replaceable_trifles].insert
-          db[:worthless_junk].insert
-        end
+          let(:db) { helper.connection }
 
-        context 'by default' do
-          it 'deletes all the tables' do
-            d = Deletion.new
-            d.db = db
-            d.clean
+          before { subject.db = db }
 
-            expect(db[:replaceable_trifles]).to be_empty
-            expect(db[:worthless_junk]).to be_empty
-            expect(db[:precious_stones]).to be_empty
+          context 'when several tables have data' do
+            before do
+              db[:users].insert
+              db[:agents].insert
+            end
+
+            context 'by default' do
+              it 'deletes all the tables' do
+                subject.clean
+
+                expect(db[:users]).to be_empty
+                expect(db[:agents]).to be_empty
+              end
+            end
           end
         end
-      end
-    end
-
-    supported_configurations = [
-      { :url => 'mysql:///', :connection_options => db_config['mysql'] },
-      { :url => 'postgres:///', :connection_options => db_config['postgres'] },
-    ]
-
-    supported_configurations.each do |config|
-      RSpec.describe "Sequel deletion (using a #{config[:url]} connection)" do
-        let(:db) { ::Sequel.connect(config[:url], config[:connection_options]) }
-
-        around do |example|
-          helper = SequelHelper.new(config)
-          helper.setup
-
-          example.run
-
-          helper.teardown
-        end
-
-        it_behaves_like 'a Sequel deletion strategy'
       end
     end
   end
