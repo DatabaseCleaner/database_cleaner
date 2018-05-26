2998bd43a1c27a473ac6d81332640f482917ab4d
Micah Geisel <micah@botandrose.com>
2018-05-26 00:00:18 -0700

    extract database_cleaner-neo4j adapter.

---
+++ adapters/database_cleaner-neo4j/lib/database_cleaner/neo4j.rb
@@ -0,0 +1,6 @@
+require "database_cleaner/neo4j/version"
+require "database_cleaner"
+require "database_cleaner/neo4j/transaction"
+require "database_cleaner/neo4j/truncation"
+require "database_cleaner/neo4j/deletion"
+
---
+++ adapters/database_cleaner-neo4j/lib/database_cleaner/neo4j/version.rb
@@ -0,0 +1,5 @@
+module DatabaseCleaner
+  module Neo4j
+    VERSION = "1.8.0"
+  end
+end
