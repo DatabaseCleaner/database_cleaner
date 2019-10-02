d9d977755eec7682f27dc7e5a40e8062d946849f
Hank Shiao <hshiao@spokeo.com>
2019-10-02 14:32:01 -0700

    Delete spec
    Pretty difficult to write an ActiveRecord 5 spec when the project is
    running ActiveRecord 3. DatabaseCleaner::ActiveRecord::Base#exclusion_condition
    wasn't previously tested anyways. ¯\_(ツ)_/¯

--- spec/database_cleaner/active_record/truncation_spec.rb
+++ spec/database_cleaner/active_record/truncation_spec.rb
@@ -47,15 +47,6 @@ RSpec.describe DatabaseCleaner::ActiveRecord::Truncation do
             expect(count).to eq 2
           end
 
-          it "should not truncate ar_internal_metadata on Rails 5" do
-            allow(::ActiveRecord::Base).to receive(:internal_metadata_table_name).and_return('ar_internal_metadata')
-            stub_const("::ActiveRecord::VERSION::MAJOR", 5)
-            expect(connection)
-              .not_to receive(:truncate_table)
-              .with(::ActiveRecord::Base.internal_metadata_table_name)
-            subject.clean
-          end
-
           it "should only truncate the tables specified in the :only option when provided" do
             expect { described_class.new(only: ['agents']).clean }
               .to change { [User.count, Agent.count] }
