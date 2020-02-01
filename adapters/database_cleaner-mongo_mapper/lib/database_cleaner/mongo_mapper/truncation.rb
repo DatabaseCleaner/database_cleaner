8012a6c5cdc6a767945b72f59bc5fb73801dd096
Micah Geisel <micah@botandrose.com>
2020-02-01 09:16:23 -0800

    inline mongo_mapper dependencies.

--- adapters/database_cleaner-mongo_mapper/lib/database_cleaner/mongo_mapper/truncation.rb
+++ adapters/database_cleaner-mongo_mapper/lib/database_cleaner/mongo_mapper/truncation.rb
@@ -1,15 +1,31 @@
 require 'database_cleaner/mongo_mapper/base'
 require 'database_cleaner/generic/truncation'
-require 'database_cleaner/mongo/truncation_mixin'
 
 module DatabaseCleaner
   module MongoMapper
     class Truncation
       include ::DatabaseCleaner::MongoMapper::Base
       include ::DatabaseCleaner::Generic::Truncation
-      include ::DatabaseCleaner::Mongo::TruncationMixin
-      
+
+      def clean
+        if @only
+          collections.each { |c| c.send(truncate_method_name) if @only.include?(c.name) }
+        else
+          collections.each { |c| c.send(truncate_method_name) unless @tables_to_exclude.include?(c.name) }
+        end
+        true
+      end
+
       private
+
+      def collections
+        database.collections.select { |c| c.name !~ /^system\./ }
+      end
+
+      def truncate_method_name
+        # This constant only exists in the 2.x series.
+        defined?(::Mongo::VERSION) ? :delete_many : :remove
+      end
 
       def database
         ::MongoMapper.database
