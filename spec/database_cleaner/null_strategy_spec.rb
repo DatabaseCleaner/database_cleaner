a5928f07b4415104aea7d1138089f7b3c9639300
Micah Geisel <micah@botandrose.com>
2020-02-04 07:45:54 -0800

    reduce usage of bare subject on advice of rspec core team.

--- spec/database_cleaner/null_strategy_spec.rb
+++ spec/database_cleaner/null_strategy_spec.rb
@@ -2,24 +2,26 @@ require "database_cleaner/null_strategy"
 
 module DatabaseCleaner
   RSpec.describe NullStrategy do
+    subject(:strategy) { NullStrategy.new }
+
     it 'responds to .start' do
-      expect { subject.start }.not_to raise_error
+      expect { strategy.start }.not_to raise_error
     end
 
     it 'responds to .clean' do
-      expect { subject.clean }.not_to raise_error
+      expect { strategy.clean }.not_to raise_error
     end
 
     describe '.cleaning' do
       it 'fails without a block' do
-        expect { subject.cleaning }.to raise_error(LocalJumpError)
+        expect { strategy.cleaning }.to raise_error(LocalJumpError)
       end
 
       it 'no-ops with a block' do
         effect = double
         expect(effect).to receive(:occur).once
 
-        subject.cleaning do
+        strategy.cleaning do
           effect.occur
         end
       end
