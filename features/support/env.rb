281d591cc8c0ff05aa496ea016c1716cfc2d75d4
Micah Geisel <micah@botandrose.com>
2020-04-24 12:50:37 -0700

    add codecov to cucumber test runs, too.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,18 @@
+require 'rspec/expectations'
+
+if ENV['COVERAGE'] == 'true'
+  require "simplecov"
+
+  if ENV['CI'] == 'true'
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+    puts "required codecov"
+  end
+
+  SimpleCov.start
+  puts "required simplecov"
+end
+
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'database_cleaner-core'
 
-require 'rspec/expectations'
